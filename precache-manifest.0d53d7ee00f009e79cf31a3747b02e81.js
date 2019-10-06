ec3c0751b4255581a75f30fb76f28d54e3a7941f
Roman <kamazaff22@gmail.com>
2019-10-06 21:46:33 +0300

    add gh-pages

---
+++ precache-manifest.0d53d7ee00f009e79cf31a3747b02e81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b79f18655e1f37ba564e046ce57c96f",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "644b338e053e0a177f84",
+    "url": "/weather-app/static/css/main.df9279a9.chunk.css"
+  },
+  {
+    "revision": "49add96b40f8c2c47301",
+    "url": "/weather-app/static/js/2.c6099e62.chunk.js"
+  },
+  {
+    "revision": "644b338e053e0a177f84",
+    "url": "/weather-app/static/js/main.f9e2bf6f.chunk.js"
+  },
+  {
+    "revision": "41f196e7b115d4760035",
+    "url": "/weather-app/static/js/runtime~main.64c1ba9b.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weather-app/static/media/polygon.cc54c19b.jpg"
+  }
+]);
--- src/redux/selectors.js
+++
@@ -1,9 +0,0 @@
-export const getData = (state) => {
-    if (state.data.selected === 'OpenWeatherMap'){
-        return state.data.OpenWeatherMap.data
-    }   
-    if (state.data.selected === 'Weatherbit'){
-        return state.data.Weatherbit.data
-    }
-    else return state.data.OpenWeatherMap.data
-}
